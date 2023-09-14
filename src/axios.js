b211fe81577f655f0f948e2f71621baafffe1eec
Zahar <zaharswork@gmail.com>
2023-09-14 10:03:25 +0530

    changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-     baseURL: 'http://localhost:8000'
+     baseURL: 'https://amazon-backend-server.vercel.app'
 });
 
 export default instance;
